7711e10699754248457d8654a8e39e0c8f965089
facu-1 <facundolucerofiorelli@gmail.com>
2020-05-07 23:22:34 -0300

    fixed login controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -43,6 +43,6 @@ class LoginController extends Controller
     public function logout()
     {
         Auth::logout();
-        return redirect('/test');
+        return redirect('/alljs');
     }
 }
